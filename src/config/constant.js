8180e6ec309b6774aa79fb22e07c15b7d352811d
Allovaro <allovaro@gmail.com>
2023-03-21 14:22:23 +0300

    API_SERVER

--- src/config/constant.js
+++ src/config/constant.js
@@ -1,8 +1,8 @@
 export const GA_ID = 'G-K7FGW03X55';
 export const BASE_TITLE = 'SVG to SVGHMI';
-export const API_SERVER = 'http://localhost:2800/api';
+// export const API_SERVER = 'http://localhost:2800/api';
 // export const API_SERVER = 'http://192.168.7.141:2800/api';
-// export const API_SERVER = 'https://server.cirillsokolov.com:2800/api';
+export const API_SERVER = 'https://server.cirillsokolov.com:2800/api';
 export const API_CRYPTOCLOUD = 'https://api.cryptocloud.plus/v1';
 export const AUTH_CRYPTOCLOUD = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MjE0NCwiZXhwIjo4ODA2NTkxNDc3Mn0.KeYgruMO8EMj9Q_vt-jw469qtHo6OozBg8kxs1RzHHY';
 export const SHOP_ID = 'W5iMrW2Dlt3zScY9';
